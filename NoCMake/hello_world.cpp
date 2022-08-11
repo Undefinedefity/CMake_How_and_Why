90e0666141b53339c127f202436ff6a52d2563a4
Undefinedefity <11711808@mail.sustech.edu.cn>
2022-08-11 14:28:36 +0800

    files without using CMake

---
+++ NoCMake/hello_world.cpp
@@ -0,0 +1,9 @@
+#include "hello_world.h"
+#include <iostream>
+
+void hello_world()
+{
+    hello();
+	world();
+    std::cout << "hello world, from hello_world.cpp\n";
+}
---
+++ NoCMake/world.cpp
@@ -0,0 +1,6 @@
+#include "world.h"
+#include <iostream>
+void world()
+{
+    std::cout << "world, from world.cpp\n";
+}
